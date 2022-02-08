3e73ac3d7e83b5ab9d735788a6e0406c82d330a6
seongkyu-lim <96x60812@naver.com>
2022-02-08 15:04:09 +0900

    add: hello world

---
+++ src/main/java/com/loopy/AcquaintanceInformationManagementSystem/AcquaintanceInformationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.loopy.AcquaintanceInformationManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquaintanceInformationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquaintanceInformationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loopy/AcquaintanceInformationManagementSystem/AcquaintanceInformationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopy.AcquaintanceInformationManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcquaintanceInformationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
